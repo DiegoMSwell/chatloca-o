7eb41e8cb70e74036dbcfb337d9dc0d7d086bffc
DiegoMSwell <diegomoises.0913@gmail.com>
2025-04-21 16:05:28 -0300

    correção do codigo com ajuda de papito

--- src/server.js
+++ src/server.js
@@ -4,10 +4,9 @@ import { prepararConexaoWhatsapp } from "./client.js"
 
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000; 
 
 app.use(express.json())
-
 app.get("/", (req, res) => {
     res.send("Diego Chat")
 })
